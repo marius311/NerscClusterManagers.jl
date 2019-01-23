8660ebf8f1c6f613e1f384b391edf9b5c5e6705c
marius <mmillea@ucdavis.edu>
2019-01-22 17:03:08 -0800

    use ephemeral port by default
    pending https://github.com/JuliaParallel/ClusterManagers.jl/pull/110

--- src/NerscClusterManagers.jl
+++ src/NerscClusterManagers.jl
@@ -5,14 +5,17 @@ import ClusterManagers: ElasticManager, IPv4
 
 export ElasticManager
 
-function ElasticManager(::Val{:nersc}; port=12345)
+function ElasticManager(::Val{:nersc}; port=0)
     ip = strip(read(`hostname --ip-address`, String))
     cookie = randstring(16)
+    
+    em = ElasticManager(addr=IPv4(ip), port=port, cookie=cookie)
+    port = convert(Int,em.sockname[2]) # port could be ephemeral, so read it again here
     
     println("To connect workers:")
     println("julia -e 'using ClusterManagers; ClusterManagers.elastic_worker(\"$cookie\",\"$ip\",$port)'")
     
-    ElasticManager(addr=IPv4(ip), port=port, cookie=cookie)
+    em
 end
 
 end
